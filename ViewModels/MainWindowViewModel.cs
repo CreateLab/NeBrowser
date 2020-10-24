f9fe7afb953ae8ec3c0e9f1be7f90b7a61343180
lary <f98f96@gmail.com>
2020-10-24 16:41:22 +0300

    first_step

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AStalker.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Hello World!";
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace AStalker.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
