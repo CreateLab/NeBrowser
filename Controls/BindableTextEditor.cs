3bfd682308a64d00c21a57ce6f3e4434357f3424
r.sorokin <r.sorokin@skbkontur.ru>
2020-11-06 17:40:10 +0300

    updaate

--- Controls/BindableTextEditor.cs
+++ Controls/BindableTextEditor.cs
@@ -7,51 +7,32 @@ using AvaloniaEdit;
 
 namespace NeBrowser.Controls
 {
-	public class BindableTextEditor: TextEditor, INotifyPropertyChanged
+	public class BindableTextEditor : TextEditor, INotifyPropertyChanged
 	{
 		/// <summary>
 		/// A bindable Text property
 		/// </summary>
-		public  string BindingText
+		private string _text;
+
+		public string BindingText
 		{
-			get => GetValue(BindingTextProperty);
+			get => _text;
 			set
 			{
 				Text = value;
-				SetValue(BindingTextProperty, value);
+				SetAndRaise(BindingTextProperty, ref _text, value);
 			}
 		}
 
 		/// <summary>
 		/// The bindable text property dependency property
 		/// </summary>
-
-		public static readonly DirectProperty<BindableTextEditor,string> BindingTextProperty =
-			AvaloniaProperty.RegisterDirect<BindableTextEditor, string>(nameof(BindingText),
-				editor => editor.BindingText,
-				(editor, s) => editor.BindingText = s,
-				default(string),BindingMode.TwoWay);
-		
-	
-
-		protected override void OnTextChanged(EventArgs e)
-		{
-			RaisePropertyChanged(nameof(BindingText));
-			base.OnTextChanged(e);
-		}
-
-		/// <summary>
-		/// Raises a property changed event
-		/// </summary>
-		/// <param name="property">The name of the property that updates</param>
-		public void RaisePropertyChanged(string property)
-		{
-			if (PropertyChanged != null)
-			{
-				PropertyChanged(this, new PropertyChangedEventArgs(property));
-			}
-		}
-
-		public event PropertyChangedEventHandler PropertyChanged;
+		public static readonly DirectProperty<BindableTextEditor, string>
+			BindingTextProperty =
+				AvaloniaProperty.RegisterDirect<BindableTextEditor, string>(
+					nameof(BindingText),
+					editor => editor.BindingText,
+					(editor, s) => editor.BindingText = s,
+					default(string), BindingMode.TwoWay);
 	}
 }
